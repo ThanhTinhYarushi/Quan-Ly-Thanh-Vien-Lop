d1e85fc24b89f2d5a89627d3574358640c4e6048
thanhemdz <bthanh1533@gmail.com>
2024-10-22 23:14:35 +0700

    cap nhat GUI

--- QuanLyThanhVien.GUI/Program.cs
+++ QuanLyThanhVien.GUI/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLyThanhVien.GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogIn());
+            Application.Run(new frmSinhVien());
         }
     }
 }

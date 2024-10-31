1215232373eb639b6d04d5c20134e105ca8f187e
thanhemdz <bthanh1533@gmail.com>
2024-10-31 22:23:01 +0700

    cap nhat from vi pham tu nhanh tinh

---
+++ QuanLyThanhVien.GUI/Students.GUI/frmXemViPham.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyThanhVien.GUI.Students.GUI
+{
+    public partial class frmXemViPham : Form
+    {
+        public frmXemViPham()
+        {
+            InitializeComponent();
+        }
+
+        private void frmXemViPham_Load(object sender, EventArgs e)
+        {
+            // load tất cả vi phạm của sinh vien
+        }
+    }
+}
